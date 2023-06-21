931eea0851ae9ab09ae4f5383985c0f7f4b6efe3
Slupshi <slupshi@gmail.com>
2023-06-21 15:29:01 +0200

    fix: git rm --cached

---
+++ App/Controllers/Home.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Articles;
+use Core\Controller;
+use Core\View;
+use Exception;
+
+/**
+ * Home controller
+ */
+class Home extends Controller
+{
+
+    /**
+     * Affiche la page d'accueil
+     *
+     * @return void
+     * @throws Exception
+     */
+    public function indexAction()
+    {
+
+        View::renderTemplate('Home/index.html', []);
+    }
+}
