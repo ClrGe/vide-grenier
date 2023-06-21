7c750063e62bc172e43ae9d8efb0b74eb18d7b49
Slupshi <slupshi@gmail.com>
2023-06-21 15:28:58 +0200

    fix: git rm --cached

--- install/init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ./develop
-docker-compose up -d --build
-cd ../release
-docker-compose up -d --build
-cd ../production
-docker-compose up -d --build
--- install/reset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ./develop
-docker-compose down --volumes 
-cd ../release
-docker-compose down --volumes 
-cd ../production
-docker-compose down --volumes 
