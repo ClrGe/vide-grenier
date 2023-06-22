281ff1242304db9e3178059f8418c7ac146bbafc
Slupshi <slupshi@gmail.com>
2023-06-22 11:38:09 +0200

    feat: update scripts

--- install/init.sh
+++ install/init.sh
@@ -3,5 +3,7 @@ cd ./develop
 docker-compose up -d --build
 cd ../release
 docker-compose up -d --build
+docker exec site-grenier-release sh -c ./pull.sh
 cd ../production
 docker-compose up -d --build
+docker exec site-grenier-prod sh -c ./pull.sh
